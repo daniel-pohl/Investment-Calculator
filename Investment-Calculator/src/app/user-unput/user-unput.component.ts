23c666620ede97214484fc6c373e308e1251c3a5
daniel-pohl <danielpohl@mail.de>
2024-11-28 18:05:53 +0100

    added user-input component

---
+++ Investment-Calculator/src/app/user-unput/user-unput.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-unput',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-unput.component.html',
+  styleUrl: './user-unput.component.css'
+})
+export class UserUnputComponent {
+
+}
