873a26e2ce287bc28897903bb13ea1a25110fc5d
daniel-pohl <danielpohl@mail.de>
2024-11-29 16:18:16 +0100

    Extracting Values with Two-Way-Binding

--- Investment-Calculator/src/app/user-unput/user-unput.component.ts
+++ Investment-Calculator/src/app/user-unput/user-unput.component.ts
@@ -9,9 +9,17 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-unput.component.css'
 })
 export class UserUnputComponent {
-
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+  
   onSubmit(){
     console.log('submitted!!');
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredDuration);
   }
 
 }
