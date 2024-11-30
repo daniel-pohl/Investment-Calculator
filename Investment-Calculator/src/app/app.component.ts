9620c1b3e3ab28b0574a4cfb91bf63e6bffb7db0
daniel-pohl <danielpohl@mail.de>
2024-11-30 18:52:29 +0100

    Calculating the annual investment data

--- Investment-Calculator/src/app/app.component.ts
+++ Investment-Calculator/src/app/app.component.ts
@@ -11,4 +11,33 @@ import { UserUnputComponent } from "./user-unput/user-unput.component";
 })
 export class AppComponent {
   title = 'Investment-Calculator';
+
+  calculateInvestmentResults(data:{
+    initialInvestment: number,
+    duration: number,
+    expectedReturn: number,
+    annualInvestment: number
+  }
+  ) {
+    const {initialInvestment, duration, expectedReturn, annualInvestment} = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+  
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+    return annualData;
+  }
 }
